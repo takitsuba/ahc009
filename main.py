0c932892525f779d4e64ff5567362147359c4106
takizawa <kei.cataract@gmail.com>
2022-03-26 16:13:00 +0900

    Add assert

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ def init_input():
     start = (start_i, start_j)
     goal = (goal_i, goal_j)
 
-    def wall_input():
-        walls = []
-        for _ in range(19):
-            w = [1] + list(map(int, list(input()))) + [1]
-            walls.append(w)
+    h_walls = []
+    for _ in range(20):
+        w = [1] + list(map(int, list(input()))) + [1]
+        assert len(w) == 21
 
-        return walls
+        h_walls.append(w)
 
-    h_walls = wall_input()
-    v_walls = wall_input()
+    assert len(h_walls) == 20
+
+    v_walls = []
+    v_walls.append([1 for _ in range(20)])
+    for _ in range(19):
+        w = list(map(int, list(input())))
+        assert len(w) == 20
+
+        v_walls.append(w)
+    v_walls.append([1 for _ in range(20)])
+    assert len(v_walls) == 21
 
     return start, goal, p, h_walls, v_walls
 
@@ -31,6 +39,8 @@ moves = ["U", "D", "L", "R"]
 def main():
     start, goal, p, h_walls, v_walls = init_input()
 
+    print(h_walls)
+    print(v_walls)
     print("".join(random.choices(moves, k=100)))
 
 
